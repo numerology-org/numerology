2015c28d8786c169dac038f5a277910acb0b966d
Ori Shem-Tov <orishemtov@gmail.com>
2024-02-11 14:26:39 +0200

    Updates

---
+++ precache-manifest.f14738101b19c1ec07d61f04559c1c07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32cba843eff5b54213bd431f864ad61",
+    "url": "/numerology/index.html"
+  },
+  {
+    "revision": "39daecd20b83a77760ce",
+    "url": "/numerology/static/css/2.998cd177.chunk.css"
+  },
+  {
+    "revision": "5f655bff96e60e9963b3",
+    "url": "/numerology/static/css/main.36eceb47.chunk.css"
+  },
+  {
+    "revision": "39daecd20b83a77760ce",
+    "url": "/numerology/static/js/2.0d648a09.chunk.js"
+  },
+  {
+    "revision": "c13ac93c0d1add0e7dfe3327568f5767",
+    "url": "/numerology/static/js/2.0d648a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f655bff96e60e9963b3",
+    "url": "/numerology/static/js/main.be69b66d.chunk.js"
+  },
+  {
+    "revision": "e50a55d7e588f77f589a",
+    "url": "/numerology/static/js/runtime-main.1d2a5462.js"
+  }
+]);
